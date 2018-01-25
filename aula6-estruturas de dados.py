d2b0ebfc96a6939d5df977484a85397c5c528cc1
herbert <hmenezes@4smart.com.br>
2018-01-25 14:35:36 -0200

    Primeiro commit

--- aula6-estruturas de dados.py
+++ aula6-estruturas de dados.py
@@ -3,6 +3,13 @@ minha_tupla = (123, 456) # Tupla: não é dinâmica (tuple)
 meu_dicionario = { 'nome' : 'Herbert', 'idade' : 27 } # key : value | é dinâmico (dict)
 meu_conjunto = { 'abc' , 'def' , 'ghi' }    # não podem ter itens repetidos | é dinâmico (set)
 
+# Estruturas vazias
+lista = []
+tupla = ()
+dicionario = {}
+conjunto = set()
+
+
 
 
 
